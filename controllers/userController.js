ab98a670afa0bcef6c008a87d3dbb668763ed5fd
kabdomora <114969831+kabdomora@users.noreply.github.com>
2023-03-07 00:25:48 -0800

    user update and delete routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,4 +21,29 @@ module.exports = {
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(500).json(err));
   },
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) => 
+        !user
+          ? res.status(404).json({ message: 'No user with this id!'})
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!'})
+          : res.json({ message: 'Thought successfully deleted!'})
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
